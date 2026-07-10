c539cc7bd9650ffc52cb97c0e72483abdfa02ef1
Stefan <stefanekfernandez@gmail.com>
2026-07-09 20:29:16 -0400

    trackID change

--- songdle/static/lib/trackIds.js
+++ songdle/static/lib/trackIds.js
@@ -2,136 +2,136 @@
 // Auto-generated from Deezer top tracks per artist - do not edit by hand.
 
 export const TRACK_IDS = [
-  1396813832, // PinkPantheress - Pain
-  82199332,   // Britney Spears - Womanizer
-  136336116,  // Bruno Mars - That's What I Like
-  630085482,  // Ke$ha - We R Who We R
-  71645431,   // Katy Perry - Roar
-  2743578151, // Sabrina Carpenter - Espresso
-  4078104921, // Olivia Rodrigo - maggots for brains
-  4022917771, // Drake - Janice STFU
-  71645436,   // Katy Perry - Dark Horse
-  3476801931, // Chappell Roan - The Subway
-  4014260341, // Kelela - the bridge
-  1124841682, // Dua Lipa - Levitating
-  2690050002, // Ariana Grande - we can't be friends (wait for your love)
-  66660698,   // Lana Del Rey - Young And Beautiful
-  14525574,   // Rihanna - We Found Love (Album Version)
-  17135110,   // Katy Perry - California Gurls
-  4022917821, // Drake - National Treasures
-  1842063487, // Beyoncé - CUFF IT
-  17135108,   // Katy Perry - Teenage Dream
-  82715364,   // Adele - Skyfall
-  959948182,  // Charli xcx - party 4 u
-  366297921,  // Dua Lipa - New Rules
-  2833834872, // Charli xcx - Apple
-  1414907322, // Lady Gaga - Judas
-  2831602002, // Sabrina Carpenter - Please Please Please
-  16149860,   // Lana Del Rey - Summertime Sadness
-  2833834822, // Charli xcx - Von dutch
-  3988410701, // Madonna - Bring Your Love
-  62710229,   // Ke$ha - Die Young
-  4131547751, // Beyoncé - MORNING DEW (DONK)
-  4045817471, // Ariana Grande - hate that i made you love me
-  1704212507, // Yung Lean - Yoshi City
-  366297871,  // Dua Lipa - IDGAF
-  62724015,   // Bruno Mars - Locked out of Heaven
-  737967292,  // Taylor Swift - Cruel Summer
-  4022917791, // Drake - Shabang
-  3400077541, // Sabrina Carpenter - Manchild
-  1976903157, // Taylor Swift - Anti-Hero
-  3529873491, // Sabrina Carpenter - Tears
-  803010392,  // Billie Eilish - everything i wanted
-  1174602992, // Adele - Rolling in the Deep
-  739421502,  // Lana Del Rey - Cinnamon Girl
-  4022917781, // Drake - Ran To Atlanta
-  1174603032, // Adele - Set Fire to the Rain
-  15595928,   // Ke$ha - Your Love Is My Drug
-  1375215242, // Ariana Grande - Into You
-  1557137132, // Adele - Oh My God
-  4033732041, // Bladee - Dolor
-  630085502,  // Ke$ha - Blow
-  2833834772, // Charli xcx - 360
-  1522223672, // Adele - Easy On Me
-  2454854745, // Chappell Roan - Red Wine Supernova
-  1692469987, // Yung Lean - Agony
-  734508702,  // Lady Gaga - Telephone
-  925118,     // Rihanna - Disturbia
-  1375215222, // Ariana Grande - Dangerous Woman
-  7216935,    // Bruno Mars - Just the Way You Are
-  79363293,   // Lana Del Rey - West Coast
-  3867905081, // Bruno Mars - Risk It All
-  3579685441, // Taylor Swift - Elizabeth Taylor
-  71403230,   // Pitbull - Timber
-  3579685451, // Taylor Swift - Opalite
-  2728070371, // Chappell Roan - Good Luck, Babe!
-  4601933,    // Lady Gaga - Bad Romance
-  2801558042, // Billie Eilish - CHIHIRO
-  2366043655, // Billie Eilish - What Was I Made For? [From The Motion Picture "Barbie"]
-  2454854845, // Chappell Roan - Pink Pony Club
-  1752612677, // Bladee - Hotel Breakfast
-  2801558032, // Billie Eilish - LUNCH
-  2929354381, // Charli xcx - Guess featuring billie eilish
-  3991144371, // Zara Larsson - Midnight Sun (Girls Trip)
-  4078104931, // Olivia Rodrigo - u + me = <3
-  136889400,  // The Weeknd - Starboy
-  1692471257, // Yung Lean - Ginseng Strip 2002
-  908604612,  // The Weeknd - Blinding Lights
-  1174604652, // Adele - Hello
-  2661514912, // Dua Lipa - Training Season
-  17135109,   // Katy Perry - Last Friday Night (T.G.I.F.)
-  124603270,  // Drake - One Dance
-  2485108,    // Beyoncé - Halo
-  3045225141, // Charli xcx - 360 featuring robyn & yung lean
-  4033732031, // Bladee - Highland Tyrant
-  1692500117, // Ecco2k - Western Union
-  2454854775, // Chappell Roan - Casual
-  629899842,  // Ariana Grande - 7 rings
-  2947516331, // Lady Gaga - Die With A Smile
-  3228090971, // Sabrina Carpenter - Busy Woman
-  4078104861, // Olivia Rodrigo - drop dead
-  3210598501, // The Weeknd - Timeless
-  3082526391, // Oklou - take me by the hand
-  3344984901, // PinkPantheress - Illegal
-  17135114,   // Katy Perry - The One That Got Away
-  82199342,   // Britney Spears - If U Seek Amy
-  82199334,   // Britney Spears - Circus
-  908604632,  // The Weeknd - Save Your Tears
-  92734438,   // Mark Ronson - Uptown Funk (feat. Bruno Mars)
-  106506512,  // The Weeknd - The Hills
-  13529563,   // Rihanna - Only Girl (In The World)
-  16149853,   // Lana Del Rey - Video Games
-  4078104881, // Olivia Rodrigo - stupid song
-  13142617,   // Britney Spears - Oops!...I Did It Again
-  930263902,  // Lana Del Rey - National Anthem
-  925108,     // Rihanna - Don't Stop The Music
-  2485118,    // Beyoncé - Single Ladies (Put a Ring on It)
-  2525537871, // Dua Lipa - Houdini
-  15589893,   // Britney Spears - Gimme More
-  2801558062, // Billie Eilish - WILDFLOWER
-  540175,     // Britney Spears - ...Baby One More Time
-  3763842212, // Bruno Mars - I Just Might
-  4764905,    // Ke$ha - TiK ToK
-  3579685431, // Taylor Swift - The Fate of Ophelia
-  4033732021, // Bladee - Versailles Flow
-  3214169391, // Lady Gaga - Abracadabra
-  2658723742, // Beyoncé - TEXAS HOLD 'EM
-  3937217881, // GENER8ION - STORM II
-  4105889521, // Charli xcx - Wink Wink
   609244,     // Beyoncé - Crazy In Love (feat. JAY-Z)
-  2454854795, // Chappell Roan - HOT TO GO!
-  925109,     // Rihanna - Breakin' Dishes
-  2801558052, // Billie Eilish - BIRDS OF A FEATHER
-  734508752,  // Lady Gaga - Paparazzi
-  136889434,  // The Weeknd - I Feel It Coming
-  1352360622, // The Weeknd - Save Your Tears (Remix)
-  4038634481, // Dua Lipa - Oye Mi Amor (feat. Fher de Maná) [Live From Mexico]
-  3344984911, // PinkPantheress - Girl Like Me
-  925106,     // Rihanna - Umbrella
-  4022917761, // Drake - Whisper My Name
-  3588926901, // PinkPantheress - Stateside + Zara Larsson
-  1692470717, // Yung Lean - Kyoto
+  1174603032, // Adele - Set Fire to the Rain
+  92734438,   // Mark Ronson - Uptown Funk (feat. Bruno Mars)
+  82199342,   // Britney Spears - If U Seek Amy
+  82715364,   // Adele - Skyfall
+  2743578151, // Sabrina Carpenter - Espresso
+  366297871,  // Dua Lipa - IDGAF
+  908604612,  // The Weeknd - Blinding Lights
+  17135108,   // Katy Perry - Teenage Dream
   4058916481, // Taylor Swift - I Knew It, I Knew You (From "Toy Story 5")
-  4078104901, // Olivia Rodrigo - honeybee
+  4078104881, // Olivia Rodrigo - stupid song
+  2525537871, // Dua Lipa - Houdini
+  1976903157, // Taylor Swift - Anti-Hero
+  3579685431, // Taylor Swift - The Fate of Ophelia
+  4601933,    // Lady Gaga - Bad Romance
+  3476801931, // Chappell Roan - The Subway
+  2658723742, // Beyoncé - TEXAS HOLD 'EM
+  3400077541, // Sabrina Carpenter - Manchild
+  4078104931, // Olivia Rodrigo - u + me = <3
+  4105889521, // Charli xcx - Wink Wink
+  366297921,  // Dua Lipa - New Rules
+  2454854745, // Chappell Roan - Red Wine Supernova
+  17135114,   // Katy Perry - The One That Got Away
+  3045225141, // Charli xcx - 360 featuring robyn & yung lean
+  925106,     // Rihanna - Umbrella
+  1414907322, // Lady Gaga - Judas
+  2690050002, // Ariana Grande - we can't be friends (wait for your love)
+  2929354381, // Charli xcx - Guess featuring billie eilish
+  803010392,  // Billie Eilish - everything i wanted
+  71645436,   // Katy Perry - Dark Horse
+  1692469987, // Yung Lean - Agony
+  1522223672, // Adele - Easy On Me
+  2366043655, // Billie Eilish - What Was I Made For? [From The Motion Picture "Barbie"]
+  1842063487, // Beyoncé - CUFF IT
+  15589893,   // Britney Spears - Gimme More
+  2833834772, // Charli xcx - 360
+  1752612677, // Bladee - Hotel Breakfast
+  2454854775, // Chappell Roan - Casual
+  4078104861, // Olivia Rodrigo - drop dead
+  14525574,   // Rihanna - We Found Love (Album Version)
+  13142617,   // Britney Spears - Oops!...I Did It Again
+  3937217881, // GENER8ION - STORM II
+  4045817471, // Ariana Grande - hate that i made you love me
+  1174602992, // Adele - Rolling in the Deep
+  106506512,  // The Weeknd - The Hills
+  4078104921, // Olivia Rodrigo - maggots for brains
+  3579685441, // Taylor Swift - Elizabeth Taylor
+  1124841682, // Dua Lipa - Levitating
+  1692500117, // Ecco2k - Western Union
+  2801558032, // Billie Eilish - LUNCH
+  3210598501, // The Weeknd - Timeless
+  16149853,   // Lana Del Rey - Video Games
+  136336116,  // Bruno Mars - That's What I Like
+  3214169391, // Lady Gaga - Abracadabra
+  1704212507, // Yung Lean - Yoshi City
+  630085502,  // Ke$ha - Blow
+  71403230,   // Pitbull - Timber
+  737967292,  // Taylor Swift - Cruel Summer
+  2454854845, // Chappell Roan - Pink Pony Club
+  540175,     // Britney Spears - ...Baby One More Time
+  2801558062, // Billie Eilish - WILDFLOWER
+  4022917771, // Drake - Janice STFU
+  734508752,  // Lady Gaga - Paparazzi
+  3588926901, // PinkPantheress - Stateside + Zara Larsson
+  15595928,   // Ke$ha - Your Love Is My Drug
+  4022917791, // Drake - Shabang
+  1692470717, // Yung Lean - Kyoto
+  4038634481, // Dua Lipa - Oye Mi Amor (feat. Fher de Maná) [Live From Mexico]
+  3228090971, // Sabrina Carpenter - Busy Woman
+  4131547751, // Beyoncé - MORNING DEW (DONK)
+  13529563,   // Rihanna - Only Girl (In The World)
+  4033732031, // Bladee - Highland Tyrant
+  630085482,  // Ke$ha - We R Who We R
+  82199332,   // Britney Spears - Womanizer
+  3991144371, // Zara Larsson - Midnight Sun (Girls Trip)
+  3344984901, // PinkPantheress - Illegal
+  930263902,  // Lana Del Rey - National Anthem
+  1375215242, // Ariana Grande - Into You
+  2454854795, // Chappell Roan - HOT TO GO!
+  2485108,    // Beyoncé - Halo
+  925118,     // Rihanna - Disturbia
+  82199334,   // Britney Spears - Circus
+  1557137132, // Adele - Oh My God
+  3867905081, // Bruno Mars - Risk It All
+  1396813832, // PinkPantheress - Pain
+  1352360622, // The Weeknd - Save Your Tears (Remix)
+  629899842,  // Ariana Grande - 7 rings
+  2661514912, // Dua Lipa - Training Season
+  2833834872, // Charli xcx - Apple
+  62710229,   // Ke$ha - Die Young
   4078104981, // Olivia Rodrigo - the cure
+  4022917821, // Drake - National Treasures
+  4022917781, // Drake - Ran To Atlanta
+  7216935,    // Bruno Mars - Just the Way You Are
+  739421502,  // Lana Del Rey - Cinnamon Girl
+  908604632,  // The Weeknd - Save Your Tears
+  734508702,  // Lady Gaga - Telephone
+  3344984911, // PinkPantheress - Girl Like Me
+  4764905,    // Ke$ha - TiK ToK
+  17135110,   // Katy Perry - California Gurls
+  925108,     // Rihanna - Don't Stop The Music
+  71645431,   // Katy Perry - Roar
+  16149860,   // Lana Del Rey - Summertime Sadness
+  3763842212, // Bruno Mars - I Just Might
+  925109,     // Rihanna - Breakin' Dishes
+  4078104901, // Olivia Rodrigo - honeybee
+  2485118,    // Beyoncé - Single Ladies (Put a Ring on It)
+  3529873491, // Sabrina Carpenter - Tears
+  136889400,  // The Weeknd - Starboy
+  4033732041, // Bladee - Dolor
+  66660698,   // Lana Del Rey - Young And Beautiful
+  4014260341, // Kelela - the bridge
+  3579685451, // Taylor Swift - Opalite
+  62724015,   // Bruno Mars - Locked out of Heaven
+  2728070371, // Chappell Roan - Good Luck, Babe!
+  1375215222, // Ariana Grande - Dangerous Woman
+  4022917761, // Drake - Whisper My Name
+  2947516331, // Lady Gaga - Die With A Smile
+  136889434,  // The Weeknd - I Feel It Coming
+  2833834822, // Charli xcx - Von dutch
+  2801558042, // Billie Eilish - CHIHIRO
+  2801558052, // Billie Eilish - BIRDS OF A FEATHER
+  3988410701, // Madonna - Bring Your Love
+  1692471257, // Yung Lean - Ginseng Strip 2002
+  17135109,   // Katy Perry - Last Friday Night (T.G.I.F.)
+  3082526391, // Oklou - take me by the hand
+  79363293,   // Lana Del Rey - West Coast
+  959948182,  // Charli xcx - party 4 u
+  4033732021, // Bladee - Versailles Flow
+  2831602002, // Sabrina Carpenter - Please Please Please
+  1174604652, // Adele - Hello
+  124603270,  // Drake - One Dance
 ]
